1c1549efc932cce1096de136c22d083a335babe1
moashraf <figoashraf@gmail.com>
2026-02-15 22:21:55 +0200

    rightchoice

--- admin/resources/views/complaints/index.blade.php
+++ admin/resources/views/complaints/index.blade.php
@@ -45,14 +45,14 @@
                                     <td>{{$val->id}}</td>
                                     <td>
                                         @if($val->userinfo)
-                                            <a href="{{route('complaintsUser',$val->userinfo->id)}}">{{$val->userinfo->name}}</a>
+                                            <a target="_blank" href="{{route('complaintsUser',$val->userinfo->id)}}">{{$val->userinfo->name}}</a>
                                         @else
                                             <span class="text-muted">N/A</span>
                                         @endif
                                     </td>
                                    <td>
                                        @if($val->aqars_id)
-                                           <a href="{{route('aqars.show',$val->aqars_id)}}">{{$val->message}}</a>
+                                           <a target="_blank" href="{{route('aqars.show',$val->aqars_id)}}">{{$val->message}}</a>
                                        @else
                                            {{$val->message}}
                                        @endif
