19f63420161db05cdbbebf7163b746fbe1bfdd5a
moashraf <figoashraf@gmail.com>
2026-02-15 22:14:09 +0200

    rightchoice

--- admin/resources/views/contact_forms/index.blade.php
+++ admin/resources/views/contact_forms/index.blade.php
@@ -25,7 +25,7 @@
 
         <div class="card">
             <div class="card-body p-0">
-              
+
 
                  <div class="card-body p-0">
                 <div class="card-body p-0">
@@ -38,7 +38,7 @@
                                 <th>الهاتف</th>
                                 <th>البريد الالكترونى</th>
                                 <th>العنوان</th>
-                              
+                                <th>التاريخ</th>
                                 <th>حدث</th>
                             </tr>
                             <!--end tr-->
@@ -52,8 +52,8 @@
                                     <td>{{$offer_type->phone}}</td>
                                     <td>{{$offer_type->email}}</td>
                                     <td>{{$offer_type->subject}}</td>
-                                   
-                                    
+                                    <td>{{$offer_type->created_at->format('Y-m-d H:i')}}</td>
+
                                     <td>@include('contact_forms.datatables_actions',['id'=>$offer_type->id])</td>
                                 </tr>
                             @endforeach
