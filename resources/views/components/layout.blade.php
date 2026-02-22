793626ed23c2731ba188439b0326e81858005509
moashraf <figoashraf@gmail.com>
2026-02-22 23:59:46 +0200

    rightchoice

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -141,7 +141,7 @@ else{
             white-space: nowrap;
             position: absolute;
             left: 60px;
-            top: 4%;
+            top: 0.5%;
             transform: translateY(-50%);
             z-index: 10;
             box-shadow: 0 4px 15px rgba(25, 106, 162, 0.35);
@@ -2511,8 +2511,9 @@ function updateStatus(id) {
 @guest
 <!-- Mobile Floating Register Button -->
 <a href="{{ URL::to(Config::get('app.locale').'/register') }}" class="mobile-register-btn" id="mobileRegBtn">
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" viewBox="0 0 24 24"><path d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V7H4v3H1v2h3v3h2v-3h3v-2H6zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
-    <span>سجّل الآن مجاناً</span>
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" viewBox="0 0 24 24">
+        <path d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V7H4v3H1v2h3v3h2v-3h3v-2H6zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
+    <span>سجل الآن مجاناً</span>
 </a>
 @endguest
 
