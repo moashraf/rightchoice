6ad875375fc273874d082443c2c562f99f96aa29
moashraf <figoashraf@gmail.com>
2026-03-21 16:41:02 +0200

    rightchoice

--- resources/views/components/ads.blade.php
+++ resources/views/components/ads.blade.php
@@ -4,7 +4,7 @@
 <div class="ads_show">
     <a target="_blank" href="{{ $randomAds->name }}">
         <img src="{{ URL::to('/').'/images/'.$randomAds->img }}" class="image-fluid w-100 mx-auto mb-5"
-             alt="">
+             style=" max-height: 300px;" alt="">
     </a>
 </div>
 @endisset
