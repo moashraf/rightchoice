8525c34ec90f62a45fa85ea6a136302df96d10bc
moashraf <figoashraf@gmail.com>
2026-08-09 22:26:43 +0300

    rightchoice

--- app/Http/Controllers/SiteHomeController.php
+++ app/Http/Controllers/SiteHomeController.php
@@ -36,12 +36,12 @@ class SiteHomeController extends Controller
 {
 
 
-  
 
 
 
 
-    
+
+
 
     public function home($lang)
 
@@ -55,16 +55,16 @@ class SiteHomeController extends Controller
         $vipAqars = aqar::where('status',1)->where('vip', 1)->with('governrateq')->with('districte')->with('subAreaa')->with('images') ->latest()->take(10)->get();
         $saleAqars = aqar::where('status',1)->where('status',1)->whereIn('offer_type', [1, 2])->with('governrateq')->with('districte')->with('subAreaa')->with('images')->latest()->take(4)->get();
         $rentAqars = aqar::where('status',1)->where('status',1)->whereIn('offer_type', [3, 4])->with('governrateq')->with('districte')->with('subAreaa')->with('images')->latest()->take(5)->get();
-       
+
         $mostRecent = aqar::where('status',1)->where('status',1)->with('governrateq')->with('districte')->with('subAreaa')->with('images')->take(5)->inRandomOrder()->get();
-       
+
              //  dd($saleAqars);
 
-        $services = Service::take(4)->get();
+        $services = Service::take(5)->get();
         $user = Auth::user();
-        
+
         $slider = Slider::all();
-        
+
 
         return view('SiteHome', compact('vipAqars', 'saleAqars', 'rentAqars', 'offers', 'mostRecent','slider', 'services'));
 
@@ -72,7 +72,7 @@ class SiteHomeController extends Controller
 
 
 
-    
+
 
 }
 
