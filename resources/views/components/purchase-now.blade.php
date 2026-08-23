d8f871b6a3541ce6dfe5414f5621dc270b54361a
moashraf <figoashraf@gmail.com>
2026-08-24 01:15:16 +0300

    rightchoice

--- resources/views/components/purchase-now.blade.php
+++ resources/views/components/purchase-now.blade.php
@@ -1,6 +1,6 @@
 @if(!auth()->check() || !auth()->user()->isCompanyAccount())
     <div class="text-center mb-3">
-        <a href="{{ route('priceBuyer', ['locale' => Config::get('app.locale')]) }}">
+        <a href="{{ route('contact-more-owners.index', ['locale' => Config::get('app.locale')]) }}">
             <img src="{{ asset('images/1 (1).jpeg') }}" class="img-fluid" alt="اشترك الآن">
         </a>
     </div>
