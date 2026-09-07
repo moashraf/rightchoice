4fad8fad0bed69b3dc6439acf8375dce20efe1da
ashraf <34096370+moashraf@users.noreply.github.com>
2026-09-08 00:45:46 +0300

    Add package discount percentage support

--- resources/views/admin_price_vips/fields.blade.php
+++ resources/views/admin_price_vips/fields.blade.php
@@ -12,6 +12,13 @@
     <small class="text-danger">{{ $errors->first('price') }}</small>
 </div>
 
+<!-- Discount Percentage Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('discount_percentage', 'Discount Percentage:') !!} <span class="text-danger">*</span>
+    {!! Form::number('discount_percentage', null, ['class' => 'form-control', 'min' => 0, 'max' => 100, 'step' => 1, 'required']) !!}
+    <small class="text-danger">{{ $errors->first('discount_percentage') }}</small>
+</div>
+
 <!-- Views Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('views', 'Views:') !!} <span class="text-danger">*</span>
