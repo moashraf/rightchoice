6f2d80271298f3582f9ca29994fb42351a2e54d4
ashraf <34096370+moashraf@users.noreply.github.com>
2026-09-11 00:07:09 +0300

    Import Laravel Validator facade

--- app/Http/Controllers/API/SocialAuthController.php
+++ app/Http/Controllers/API/SocialAuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use InvalidArgumentException;
 use DomainException;
