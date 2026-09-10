71d18a09dac2549a439e97194de70d403385323d
ashraf <34096370+moashraf@users.noreply.github.com>
2026-09-11 01:00:05 +0300

    Configure Apple web client credentials

--- config/services.php
+++ config/services.php
@@ -15,7 +15,11 @@ return [
     */
 
     'google' => ['web_client_id' => env('GOOGLE_WEB_CLIENT_ID')],
-    'apple' => ['client_id' => env('APPLE_CLIENT_ID')],
+    'apple' => [
+        'client_id' => env('APPLE_CLIENT_ID'),
+        'web_client_id' => env('APPLE_WEB_CLIENT_ID'),
+        'redirect_uri' => env('APPLE_REDIRECT_URI'),
+    ],
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
