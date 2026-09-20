b27d0a4f996d8b9d797b8396ba46572a6883994c
moashraf <figoashraf@gmail.com>
2026-09-20 23:15:00 +0300

    rightchoice

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('promotions:expire')->everyMinute()->withoutOverlapping();
+        $schedule->command('promotions:expire')->daily()->withoutOverlapping();
         $schedule->command('properties:suspend-expired')->daily()->withoutOverlapping();
     }
 
