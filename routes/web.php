60eccf7867ddb41811e64a1916a14fcfbc9e4840
ashraf <34096370+moashraf@users.noreply.github.com>
2026-09-22 22:59:56 +0300

    Serve app download page directly at /app

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use App\Http\Controllers\Auth\CustomPasswordResetLinkController;
 |
 */
 
+// Public app download page: keep /app independent of the optional locale prefix.
+Route::get('/app', function () { return view('app-download'); })->name('app.download');
+
 Route::get('/', 'App\Http\Controllers\SiteHomeController@home');
 Route::get('/changeLang/{url}', 'App\Http\Controllers\PageController@changeLang')->name('changeLang');
 
